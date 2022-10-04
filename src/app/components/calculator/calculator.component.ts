c1efe830e8001304c42a183433cf8efc9540e89d
codster76 <codster761@gmail.com>
2022-10-05 05:21:32 +1100

    Implemented the static factory pattern for constructor overloads in the operation class

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -6,38 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-  operation1: Operation = new Operation([new Operation([new Operation(6, OperationType.Add), new Operation(4, OperationType.Subtract)], OperationType.None), new Operation(2,OperationType.Multiply)], OperationType.None);
-  operation2: Operation = new Operation(123, OperationType.Divide);
-  operation3: Operation = new Operation([new Operation(10, OperationType.Add)], OperationType.Divide);
-  operation4: Operation = new Operation(
+  operation1: Operation = Operation.operationArray([Operation.operationArray([
+    Operation.singleValue(6, OperationType.Add),
+    Operation.singleValue(4, OperationType.Subtract)
+  ]), Operation.singleValue(2,OperationType.Multiply)]);
+
+  operation2: Operation = Operation.singleValue(123, OperationType.Divide);
+
+  operation3: Operation = Operation.operationArray([
+    Operation.singleValue(10, OperationType.Add)
+  ]);
+
+  operation4: Operation = Operation.operationArray(
     [
-      new Operation(3, OperationType.Add),
-      new Operation(4, OperationType.Multiply),
-      new Operation(2, OperationType.None),
-    ], OperationType.None
-  );
-  operation5: Operation = new Operation(
+      Operation.singleValue(3, OperationType.Add),
+      Operation.singleValue(4, OperationType.Multiply),
+      Operation.singleValue(2, OperationType.None),
+    ]);
+
+  operation5: Operation = Operation.operationArray(
     [
-      new Operation([
-        new Operation(8, OperationType.Divide),
-        new Operation(4, OperationType.Add),
-        new Operation([
-          new Operation(14, OperationType.Divide),
-          new Operation(2, OperationType.Add),
-          new Operation(3, OperationType.Subtract),
-        ], OperationType.None),
-      ], OperationType.None),
-      new Operation(4, OperationType.Multiply),
-      new Operation(2, OperationType.None),
-    ], OperationType.None
-  );
-  //((8 / 4 + (14 / 2 + 3 -)) 4 * 2) = 4
-  operation6: Operation = new Operation(
-    [
-      new Operation(5, OperationType.Divide),
-      new Operation(10, OperationType.None),
-    ], OperationType.None
-  )
+      Operation.operationArray([
+        Operation.singleValue(8, OperationType.Divide),
+        Operation.singleValue(4, OperationType.Add),
+        Operation.operationArray([
+          Operation.singleValue(14, OperationType.Divide),
+          Operation.singleValue(2, OperationType.Add),
+          Operation.singleValue(3, OperationType.Subtract),
+        ]),
+      ]),
+      Operation.singleValue(4, OperationType.Multiply),
+      Operation.singleValue(2, OperationType.None),
+    ]);
+  // //((8 / 4 + (14 / 2 + 3 -)) 4 * 2) = 4
+
+  operation6: Operation = Operation.operationArray([
+      Operation.singleValue(5, OperationType.Divide),
+      Operation.singleValue(10, OperationType.None),
+    ])
 
   constructor() { }
 
@@ -45,7 +51,7 @@ export class CalculatorComponent implements OnInit {
   }
 
   someFunction() {
-    console.log(this.calculate(this.operation6));
+    console.log(this.calculate(this.operation5));
   }
 
   calculate(operationToCalculate: Operation) {
@@ -56,23 +62,26 @@ export class CalculatorComponent implements OnInit {
     const valueAsArray: Operation[] = operationToCalculate.value as Operation[]; // The if statement confirms that it's an operation array. This is just for convenience.
 
     if(valueAsArray.length === 0) {
-      return new Operation(NaN, OperationType.None);
+      return Operation.singleValue(NaN, OperationType.None);
     }
 
     if(valueAsArray.length === 1) {
-      return new Operation(valueAsArray[0].value, valueAsArray[0].operationForNext);
+      return Operation.singleValue(valueAsArray[0].value as number, valueAsArray[0].operationSymbol);
     }
 
     while(valueAsArray.length > 1) {
       // First pass for multiplication/division
       for(let i = 0; i < valueAsArray.length-1; i++) {
-        if(valueAsArray[i].operationForNext === OperationType.Multiply || valueAsArray[i].operationForNext === OperationType.Divide) {
+        if(valueAsArray[i].operationSymbol === OperationType.Multiply || valueAsArray[i].operationSymbol === OperationType.Divide) {
+          // Basically, look at two adjacent values and check whether they are single values or brackets. If they are in brackets, compute whatever's in the brackets first. Ensures that the deepest levels of brackets always get calculated first.
           if(!valueAsArray[i].isNumber) {
             valueAsArray[i] = this.calculate(valueAsArray[i]);
           }
           if(!valueAsArray[i+1].isNumber) {
             valueAsArray[i+1] = this.calculate(valueAsArray[i+1]);
           }
+
+          // Pull the two values you're calculating out of the array and compute them
           const operation1 = valueAsArray.splice(i,1)[0];
           const operation2 = valueAsArray.splice(i,1)[0];
     
@@ -80,7 +89,7 @@ export class CalculatorComponent implements OnInit {
           const arrayValue2 = operation2?.value as number;
     
           let calculatedNumber = 0;
-          switch(operation1.operationForNext) {
+          switch(operation1.operationSymbol) {
             case OperationType.Multiply:
               calculatedNumber = arrayValue1 * arrayValue2;
               break;
@@ -88,11 +97,13 @@ export class CalculatorComponent implements OnInit {
               calculatedNumber = arrayValue1 / arrayValue2;
               break;
           }
-          valueAsArray.splice(i, 0, new Operation(calculatedNumber, operation2.operationForNext));
+
+          // Once the values have been computed, put them back in the spot they were taken from
+          valueAsArray.splice(i, 0, Operation.singleValue(calculatedNumber, operation2.operationSymbol));
         }
       }
 
-      // Second pass for addition/subtraction
+      // Second pass for addition/subtraction. Basically the same as before, but without the multiplication/division check.
       for(let i = 0; i < valueAsArray.length-1; i++) {
         if(!valueAsArray[i].isNumber) {
           valueAsArray[i] = this.calculate(valueAsArray[i]);
@@ -107,7 +118,7 @@ export class CalculatorComponent implements OnInit {
         const arrayValue2 = operation2?.value as number;
   
         let calculatedNumber = 0;
-        switch(operation1.operationForNext) {
+        switch(operation1.operationSymbol) {
           case OperationType.Add:
             calculatedNumber = arrayValue1 + arrayValue2;
             break;
@@ -115,38 +126,38 @@ export class CalculatorComponent implements OnInit {
             calculatedNumber = arrayValue1 - arrayValue2;
             break;
         }
-        valueAsArray.splice(i, 0, new Operation(calculatedNumber, operation2.operationForNext));
+        valueAsArray.splice(i, 0, Operation.singleValue(calculatedNumber, operation2.operationSymbol));
       }
     }
 
-    return new Operation(valueAsArray[0].value, valueAsArray[0].operationForNext);
+    return Operation.singleValue(valueAsArray[0].value as number, valueAsArray[0].operationSymbol);
   }
-
 }
 
 class Operation {
   value: Operation[] | number = [];
-  operationForNext: OperationType;
-  isNumber: boolean;
+  operationSymbol: OperationType = OperationType.None;
+  isNumber: boolean = false;
 
-  constructor(values: Operation[] | number, operationType: OperationType) {
-    this.value = values;
-    this.operationForNext = operationType;
-    
-    if(typeof values === 'number') {
-      this.isNumber = true;
-    } else {
-      // I have to do this jankiness because you can't define more than one constructor in javascript
-      this.operationForNext = OperationType.None;
-      this.isNumber = false;
-    }
+  static singleValue(value: number, operationType: OperationType): Operation {
+    const operation = new Operation();
+    operation.value = value;
+    operation.operationSymbol = operationType;
+    operation.isNumber = true;
+    return operation;
+  }
+
+  static operationArray(values: Operation[]): Operation {
+    const operation = new Operation();
+    operation.value = values;
+    return operation;
   }
 }
 
 enum OperationType {
-  Add = "add",
-  Subtract = "subtract",
-  Multiply = "multiply",
-  Divide = "divide",
-  None = "none",
+  Add,
+  Subtract,
+  Multiply,
+  Divide,
+  None,
 }
